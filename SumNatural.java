141ba3d0794ed9dc6e3a5411bdd4db74ef4b9b20
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-05 09:40:16 +0530

    Naam Simran Singh Add: Sum of N Natural Numbers

--- SumNatural.java
+++ SumNatural.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class SumNatural{
+	// Method to add N Natural numbers
 	public static int sum(int number){
 		int add = 0;
 		for(int i=1;i<=number;i++){
@@ -13,6 +14,6 @@ class SumNatural{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the number: ");
 		int number = sc.nextInt();
-		System.out.print("The sum is "+sum(number));
+		System.out.print("The sum is "+sum(number));// calling the method and displaying result
 	}
 }
