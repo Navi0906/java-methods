7df5898971c02f57945aa6d19bb033feb3f51fc9
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-05 11:51:00 +0530

    Naam Simran Singh Add: Calculating the trignometric functions of angles

---
+++ TrignometricFunctions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TrigonometricFunctions {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the angle in degrees: ");
+        double angle = sc.nextDouble();
+
+        // Calculating trigonometric functions
+        double[] results = calculateFunctions(angle);
+
+        System.out.println("Sine of "+angle+" degrees: "+results[0]);
+        System.out.println("Cosine of "+angle+" degrees: "+results[1]);
+        System.out.println("Tangent of "+angle+" degrees: "+results[2]);
+
+    }
+
+    public static double[] calculateFunctions(double angle) {
+        // Convert degrees to radians
+        double radians = Math.toRadians(angle);
+
+        // Calculate sine, cosine, and tangent
+        double sine = Math.sin(radians);
+        double cosine = Math.cos(radians);
+        double tangent = Math.tan(radians);
+
+        return new double[]{sine, cosine, tangent};
+    }
+}
