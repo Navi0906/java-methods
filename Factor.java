6142ade08c8e9ba33b6b5c7221739bc8c1033edc
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-05 13:48:18 +0530

    Naam Simran Singh Add: Calculating factors of given number

---
+++ Factor.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Factor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Finding factors and storing them in an array
+        int[] factors = findFactors(number);
+
+        // Displaying the factors
+        System.out.print("Factors of " + number + ": ");
+        for (int factor : factors) {
+            System.out.print(factor + " ");
+        }
+        System.out.println();
+
+        // Calculating and displaying the results
+        int sum = sumOfFactors(factors);
+        long product = productOfFactors(factors);
+        double sumOfSquares = sumOfSquaresOfFactors(factors);
+
+        System.out.println("Sum of factors: " + sum);
+        System.out.println("Product of factors: " + product);
+        System.out.println("Sum of squares of factors: " + sumOfSquares);
+
+    }
+
+    // Method to find factors of a number and return them in an array
+    public static int[] findFactors(int number) {
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index] = i;
+                index++;
+            }
+        }
+
+        return factors;
+    }
+
+    // Method to find the sum of the factors
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    // Method to find the product of the factors
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    // Method to find the sum of squares of the factors
+    public static double sumOfSquaresOfFactors(int[] factors) {
+        double sumOfSquares = 0;
+        for (int factor : factors) {
+            sumOfSquares += Math.pow(factor, 2);
+        }
+        return sumOfSquares;
+    }
+}
